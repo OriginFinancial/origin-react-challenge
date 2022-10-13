032c3ff4c816a973abc8095350ff9186ea4d6652
Clarice Bianchini <claricebbcosta@gmail.com>
2022-10-13 10:27:09 -0300

    refator: removes css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import './App.css';
 import { usePokemonApi } from './hooks/usePokemonApi';
 import { Header } from './components/Header';
 import { Button } from './components/Button';
