be252b2d659839efcdcd97b4b275c63c54887d8a
Clarice Bianchini <claricebbcosta@gmail.com>
2022-11-03 10:32:11 -0300

    test: adds test describes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  describe('when rendering', () => {
+    it('adds renders list of pokemons', () => {
+      render(<App />);
+
+    });
+  });
+  describe('when clicking on load more', () => {
+    it('loads another set of pokemons', () => {
+      render(<App />);
+
+      fireEvent.click(screen.queryByText('Load more'))
+
+      expect(true).toBeTruthy();
+    });
+  });
 });
+
